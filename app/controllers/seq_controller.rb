038f023fa9d046bc16b24ca2cc2cbb94f112acca
RomchAss <gameathome2015@gmail.com>
2022-11-15 05:15:50 +0300

    in each td a digit

--- app/controllers/seq_controller.rb
+++ app/controllers/seq_controller.rb
@@ -3,5 +3,39 @@ class SeqController < ApplicationController
   end
 
   def view
+    @count=0
+    n=params[:num].to_i
+    seq=params[:str].chomp.split(' ').map{|each| each.to_i}
+    
+    $all_seqs=Array.new
+    $curr_seq=Array.new
+    $match=false
+    
+    def when_not_match()
+        $all_seqs.push($curr_seq.clone)
+        $curr_seq.clear
+        $match=false
+    end
+    
+    seq.each_index do |ind|    
+        num=seq[ind]
+        sum=0
+        1.upto(num/2) do |del|
+            if num%del==0 then sum+=del
+            end
+        end
+        if num==sum
+            @count+=1
+            $curr_seq.push(num.clone)
+            $match=true
+        elsif $match
+            when_not_match
+        end    
+    end
+    
+    if $match
+        when_not_match
+    end
+    @result=$all_seqs
   end
 end
